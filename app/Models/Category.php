f50f5b27a529190d2d70cbf30aeeca6dd34936c9
paul <paulnyoiken@gmail.com>
2025-02-26 02:54:32 +0300

    :package: feat: Added Category model with migration

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function clothingItems()
+    {
+        return $this->hasMany(ClothingItem::class);
+    }
+}
+
